a1b79d50e055a72eb81491182c545b2832dc5bf4
ninjai74 <sean@fiv4lab.com>
2019-01-21 20:05:11 -0500

    new work

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.model import listing
+
 def index(request):
     return render(request, 'pages/index.html')
 
