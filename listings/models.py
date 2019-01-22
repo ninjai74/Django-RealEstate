8e22f82d0163f02ba6969caf16692efe65c3c016
ninjai74 <sean@fiv4lab.com>
2019-01-21 20:04:38 -0500

    sending changes

--- listings/models.py
+++ listings/models.py
@@ -7,7 +7,7 @@ class listing(models.Model):
     title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
-    sate = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
     zipcode = models.CharField(max_length=20)
     description = models.TextField(blank=True)
     price = models.IntegerField()
